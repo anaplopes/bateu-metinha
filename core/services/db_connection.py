843d8fbfe176aee593fa9df60cb218dbf2ea7300
Ana Paula Lopes <30847848+anaplopes@users.noreply.github.com>
2020-12-02 14:33:18 -0300

    corrigido server

--- core/services/db_connection.py
+++ core/services/db_connection.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
 from pymongo import MongoClient
+import config
 
 
 class DbConnectionService:
     """ Serviço responsável por cria e fechar conexão com o database. """
 
     def __init__(self):
+        self.__settings = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
         self.__mongo_db = os.getenv('MONGO_DB', '')
-        self.client = MongoClient(os.getenv('PROD_DATABASE_URI', ''))
+        self.client = MongoClient(eval(self.__settings).DATABASE_URI)
     
     def create_connection(self, collection):
         dbs = self.client.list_database_names()
